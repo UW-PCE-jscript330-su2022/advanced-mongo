54151c5d883cf3de705fd22a933845b15e5ecff4
Joann Cahill <cahilljm@aol.com>
2022-07-22 18:05:59 -0700

    Push base code confirmed working

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,7 @@ const router = Router();
 
 const movieData = require('../dataInterface/movies');
 
+// GET ALL MOVIES
 // curl -sS http://localhost:5001/movies
 router.get("/", async (req, res, next) => {
   let movieList = await movieData.getAll()
@@ -15,6 +16,7 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+// GET MOVIES BY TITLE OR ID
 // This route handles either id or title as an identifier.
 // curl -sS http://localhost:5001/movies/573a1390f29313caabcd4135
 // curl -sS http://localhost:5001/movies/Jurassic%20Park
@@ -31,12 +33,15 @@ router.get("/:id", async (req, res, next) => {
 
 });
 
-//COMMENTS FOR A MOVIE
-router.get("/:id/comments", async(req, res) => {
-console.log("GET comments for a movie:" , req.params.id)
-res.status(200).send("ok");
-})
+// GET COMMENTS FOR A MOVIE
+// // curl -sS http://localhost:5001/movies/000/comments
+// router.get("/:id/comments", async(req, res) => {
+//   const result = await movieData.getAllComments(req.params.id)
+//   res.status(200).send(result);
+// })
 
+
+// CREATE A NEW MOVIE
 // curl -sS -X POST -H "Content-Type: application/json" -d '{"title":"Llamas From Space", "plot":"Aliens..."}' http://localhost:5001/movies
 router.post("/", async (req, res, next) => {
   let resultStatus;
@@ -51,14 +56,16 @@ router.post("/", async (req, res, next) => {
   res.status(resultStatus).send(result);
 });
 
-//CREATE COMMENTS FOR A MOVIE
-router.put("/:id/comments", async(req, res) => {
-  console.log("CREATE comments for a movie:" , req.params.id)
-  res.status(200).send("ok");
-  })
+// //CREATE A NEW COMMENT
+// // curl -sS -X POST -H "Content-Type: application/json" -d '{"name":"Cinephile Cyprus", "text":"Wow!"}' http://localhost:5001/movies/000/comments
+// router.post("/:id/comments", async(req, res) => {
+//   const result = await movieData.createComment(req.params.id, req.body)
+//   res.status(200).send(result);
+// })
 
 
-// curl -sS -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5001/movies/62d75e75432b3bdb436148cb
+// UPDATE A MOVIE PLOT
+// curl -sS -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5001/movies/62db489c4d8c10f61cf878e0
 router.put("/:id", async (req, res, next) => {
   let resultStatus;
   const result = await movieData.updateById(req.params.id, req.body)
@@ -72,7 +79,9 @@ router.put("/:id", async (req, res, next) => {
   res.status(resultStatus).send(result);
 });
 
-// curl -sS -X DELETE http://localhost:5001/movies/curl -sS -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5001/movies/62d75e75432b3bdb436148cb
+
+// DELETE A MOVIE
+// curl -sS -X DELETE http://localhost:5001/movies/62db489c4d8c10f61cf878e0
 router.delete("/:id", async (req, res, next) => {
   const result = await movieData.deleteById(req.params.id);
 
@@ -85,99 +94,11 @@ router.delete("/:id", async (req, res, next) => {
   res.status(resultStatus).send(result);
 });
 
-//DELETE COMMENTS FOR A MOVIE
-const { Router } = require("express");
-const router = Router();
-
-const movieData = require('../dataInterface/movies');
-
-// curl http://localhost:5000/movies
-router.get("/", async (req, res, next) => {
-  let movieList = await movieData.getAll()
-
-  if(movieList){
-    res.status(200).send(movieList)
-  } else {
-    // If movieList is empty/null, something serious is wrong with the MongoDB connection.
-    res.status(500).send({error: "Something went wrong. Please try again."})
-  }
-});
-
-// This route handles either id or title as an identifier.
-// curl http://localhost:5000/movies/573a1390f29313caabcd4135
-// curl http://localhost:5000/movies/Jurassic%20Park
-router.get("/:id", async (req, res, next) => {
-  const result = await movieData.getByIdOrTitle(req.params.id)
-
-  if(result.error){
-    resultStatus = 404;
-  } else {
-    resultStatus = 200;
-  }
-
-  res.status(resultStatus).send(result);
-
-});
-
-// GET ALL COMMENTS FOR A MOVIE
-router.get("/:id/comments", async(req, res) => {
-  console.log("GET comments for a movie: ", req.params.id)
-  res.status(200).send("ok :)");
-})
-
-// curl -X POST -H "Content-Type: application/json" -d '{"title":"Llamas From Space", "plot":"Aliens..."}' http://localhost:5000/movies
-router.post("/", async (req, res, next) => {
-  let resultStatus;
-  let result = await movieData.create(req.body);
-
-  if(result.error){
-    resultStatus = 400;
-  } else {
-    resultStatus = 200;
-  }
-
-  res.status(resultStatus).send(result);
-});
-
-// CREATE A NEW COMMENT
-router.post("/:id/comments", async(req, res) => {
-  console.log("CREATE a comment for a movie: ", req.params.id)
-  res.status(200).send("ok :)");
-})
-
-// curl -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5000/movies/573a13a3f29313caabd0e77b
-router.put("/:id", async (req, res, next) => {
-  let resultStatus;
-  const result = await movieData.updateById(req.params.id, req.body)
-
-  if(result.error){
-    resultStatus = 400;
-  } else {
-    resultStatus = 200;
-  }
-
-  res.status(resultStatus).send(result);
-});
-
-// curl -X DELETE http://localhost:5000/movies/573a1390f29313caabcd4135
-router.delete("/:id", async (req, res, next) => {
-  const result = await movieData.deleteById(req.params.id);
-
-  if(result.error){
-    resultStatus = 400;
-  } else {
-    resultStatus = 200;
-  }
-
-  res.status(resultStatus).send(result);
-});
-
-// DELETE A COMMENT
-router.delete("/:movieId/comments/:commentId", async(req, res)=>{
-  console.log("Deleting a comment with id: ", req.params.commentId)
-  res.status(200).send("ok :)");
-})
-
-module.exports = router;
+// // DELETE A MOVIE COMMENT
+// // curl -sS -X DELETE http://localhost:5000/movies/000/comments/000
+// router.delete("/:movieId/comments/:commentId", async(req, res)=>{
+//   const result = await movieData.deleteCommentById(req.params.commentId)
+//   res.status(200).send(result);
+// })
 
 module.exports = router;
--- routes/movies.test.js
+++ routes/movies.test.js
@@ -1,9 +1,8 @@
 const request = require("supertest");
 const server = require("../server");
 
-// Declare that jest will mock movieData.  Must be before the require statement.
-
-jest.mock("...dataInterface/movies")
+// Declare the jest will mock movieData. Must be before the require statement.
+jest.mock("../dataInterface/movies");
 const movieData = require("../dataInterface/movies")
 
 describe("/movies routes", () => {
@@ -18,17 +17,17 @@ describe("/movies routes", () => {
 
       const res = await request(server).get("/movies");
 
-      expect(res.statusCode).toEqual(500);
-      expect(res.body.error).toBeDefined();
-    });
-    it("should return an error message on error", async () => {
-      // expect(false).toEqual(true);
-      movieData.getAll.mockResolvedValue(null);
-      const res = await request(server).get("/movies");
-
       expect(res.statusCode).toEqual(200);
       expect(Array.isArray(res.body)).toEqual(true);
       expect(res.body.error).not.toBeDefined();
+    });
+    it("should return an error message on error", async () => {
+      movieData.getAll.mockResolvedValue(null);
+
+      const res = await request(server).get("/movies");
+
+      expect(res.statusCode).toEqual(500);
+      expect(res.body.error).toBeDefined();
     });
   });
 
@@ -36,7 +35,7 @@ describe("/movies routes", () => {
     it("should return a single movie on success", async () => {
       expect(false).toEqual(true);
     });
-    it("should return a status code of 404 if not found", async () => {
+    it("should return a status code of 404 if movie not found", async () => {
       expect(false).toEqual(true);
     });
   });
@@ -45,19 +44,21 @@ describe("/movies routes", () => {
     it("should return the new movie on success", async () => {
       expect(false).toEqual(true);
     });
-    it("should return a 404 error message if body is missing title", async () => {
+    it("should return an error message if body is missing title", async () => {
       expect(false).toEqual(true);
+      // expect status code == 400
     });
-    it("should return a 404 error message if movie fails to be created", async () => {
+    it("should return an error message if movie fails to be created", async () => {
       expect(false).toEqual(true);
+      // expect status code == 400
     });
   });
 
   describe("PUT /:id", () =>{
-    it("should return a message on success", async () => {
+    it("should return the updated movie on success", async () => {
       expect(false).toEqual(true);
     });
-    it("should return an error code 400 if a record is not updated", async () => {
+    it("should return an error message if movie fails to be updated", async () => {
       expect(false).toEqual(true);
     });
   });
@@ -66,8 +67,36 @@ describe("/movies routes", () => {
     it("should return a message on success", async () => {
       expect(false).toEqual(true);
     });
-    it("should return an error message if the deletion fails", async () => {
+    it("should return a error message if movie fails to be deleted", async () => {
       expect(false).toEqual(true);
     });
   });
+
+    describe("GET /movies/genres/:genreName", () =>{
+    it("should return an array of movies on success", async () => {
+      // TODO: Mock the correct data interface method
+      const res = await request(server).get("/movies/genres/Short");
+
+      expect(res.statusCode).toEqual(200);
+      expect(Array.isArray(res.body)).toEqual(true);
+      expect(res.body.error).not.toBeDefined();
+    });
+    it("should return an empty array if no movies match genre", async () => {
+      // TODO: Mock the correct data interface method
+      const res = await request(server).get("/movies/genres/UEOA921DI");
+
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toEqual(0);
+      expect(res.body.error).not.toBeDefined();
+    });
+    it("should return an error message on error", async () => {
+      // TODO: Mock the correct data interface method
+
+      const res = await request(server).get("/movies/genres/Short");
+
+      expect(res.statusCode).toEqual(500);
+      expect(res.body.error).toBeDefined();
+    });
+  });
+
 });
