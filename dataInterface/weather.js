2b5577d70b845261bcc74096bf0d22ccc58a29c4
pablomarcel <pablomarcel@outlook.com>
2022-07-30 00:27:42 -0700

    get callLetters working

--- dataInterface/weather.js
+++ dataInterface/weather.js
@@ -8,8 +8,6 @@ const client = new MongoClient(uri);
 const databaseName = 'sample_weatherdata';
 const collName = 'data'
 
-
-
 module.exports = {}
 
 // https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
@@ -21,84 +19,39 @@ module.exports.getAll = async () => {
     return weatherCursor.toArray();
 }
 
-module.exports.getAllComments = async (movieId)=>{
-    const database = client.db(databaseName);
-    const comments = database.collection(commCollName);
-    const query = { movie_id: ObjectId(movieId)}
-    let commentCursor = await  comments.find(query)
-    return commentCursor.toArray()
-}
-
-module.exports.getCommentById = async (commentId) => {
-    const database = client.db(databaseName);
-    const comments = database.collection(commCollName);
-    const query = {_id: ObjectId(commentId)};
-    let comment = await comments.findOne(query);
-
-    return comment;
-}
-
-module.exports.getAComment = async (identifier) => {
-    let comment;
-
-    if(ObjectId.isValid(identifier)){
-        comment = module.exports.getCommentById(identifier);
-    }
-
-    if(comment){
-        return comment;
-    } else {
-        return {error: `No comment found with identifier ${identifier}.`}
-    }
-}
-
 // https://www.mongodb.com/docs/drivers/node/current/usage-examples/findOne/
-module.exports.getById = async (movieId) => {
+module.exports.getById = async (weatherId) => {
     const database = client.db(databaseName);
-    const movies = database.collection(collName);
-    const query = {_id: ObjectId(movieId)};
-    let movie = await movies.findOne(query);
+    const weather = database.collection(collName);
+    const query = {_id: ObjectId(weatherId)};
+    let record = await weather.findOne(query);
 
-    return movie;
+    return record;
 }
 
-module.exports.getByTitle = async (title) => {
+module.exports.getByCallLetter = async (identity) => {
+    //console.log(identity)
     const database = client.db(databaseName);
-    const movies = database.collection(collName);
-    const query = {title: title};
-    let movie = await movies.findOne(query);
-
-    return movie;
+    const weather = database.collection(collName);
+    const records = await weather.find({callLetters: {$eq: identity}}).limit(10)
+    return records.toArray();
 }
 
-module.exports.getByIdOrTitle = async (identifier) => {
-    let movie;
+module.exports.getByIdOrCallLetter = async (identifier) => {
+    //console.log(identifier)
+    let records;
 
     if(ObjectId.isValid(identifier)){
-        movie = module.exports.getById(identifier);
+        records = module.exports.getById(identifier);
     } else {
-        movie = module.exports.getByTitle(identifier);
+        records = module.exports.getByCallLetter(identifier);
     }
 
-    if(movie){
-        return movie;
+    if(records){
+        return records;
     } else {
         return {error: `No item found with identifier ${identifier}.`}
     }
-}
-
-module.exports.createComment = async (movieId, newObj) =>{
-
-    const database = client.db(databaseName);
-    const comments = database.collection(commCollName);
-    const goodObj = {...newObj, movie_id: ObjectId(movieId), date: new Date() }
-    const result = await comments.insertOne(goodObj)
-    if(result.acknowledged){
-        return { newObjectId: result.insertedId, message: `Comment created! ID: ${result.insertedId}` }
-    } else {
-        return {error: "Something went wrong. Please try again."}
-    }
-
 }
 
 // https://www.mongodb.com/docs/v4.4/tutorial/insert-documents/
@@ -117,71 +70,4 @@ module.exports.create = async (newObj) => {
     } else {
         return {error: "Something went wrong. Please try again."}
     }
-}
-
-// https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/write-operations/change-a-document/
-module.exports.updateById = async (movieId, newObj) => {
-    const database = client.db(databaseName);
-    const movies = database.collection(collName);
-
-    // Product team says only these two fields can be updated.
-    const updateRules = {
-        $set: {"title" : newObj.title, "plot": newObj.plot}
-    };
-    const filter = { _id: ObjectId(movieId) };
-    const result = await movies.updateOne(filter, updateRules);
-
-    if(result.modifiedCount != 1){
-        return {error: `Something went wrong. ${result.modifiedCount} movies were updated. Please try again.`}
-    }
-
-    const updatedMovie = module.exports.getById(movieId);
-    return updatedMovie;
-}
-
-module.exports.updateCommentById = async (commentId, newObj) => {
-    const database = client.db(databaseName);
-    const comments = database.collection(commCollName);
-
-    // Product team says only these two fields can be updated.
-    const updateRules = {
-        //$set: {"post" : newObj.post, "date": new Date()}
-        $set: {"post" : newObj.post, "date": new Date()}
-    };
-    const filter = { _id: ObjectId(commentId) };
-    const result = await comments.updateOne(filter, updateRules);
-
-    if(result.modifiedCount !== 1){
-        return {error: `Something went wrong. ${result.modifiedCount} comments were updated. Please try again.`}
-    }
-
-    const updatedComment = module.exports.getCommentById(commentId);
-    return updatedComment;
-}
-
-module.exports.deleteCommentById = async (commentId)=>{
-    const database = client.db(databaseName)
-    const comments = database.collection(commCollName)
-    const deletionRules = {_id:ObjectId(commentId)}
-    const result = await comments.deleteOne(deletionRules)
-    if(result.deletedCount !==1){
-        return {error: `Something went wrong. ${result.deletedCount} comments were deleted. Please try again`}
-    }
-
-    return {message: `Deleted ${result.deletedCount} comment.`}
-}
-
-// https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/write-operations/delete/
-module.exports.deleteById = async (movieId) => {
-    const database = client.db(databaseName);
-    const movies = database.collection(collName);
-
-    const deletionRules = {_id:ObjectId(movieId)}
-    const result = await movies.deleteOne(deletionRules);
-
-    if(result.deletedCount != 1){
-        return {error: `Something went wrong. ${result.deletedCount} movies were deleted. Please try again.`}
-    };
-
-    return {message: `Deleted ${result.deletedCount} movie.`};
 }
